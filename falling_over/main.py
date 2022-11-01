8879e23aef5e1e21ef0d90b172a9db497825d77a
ruby <jhonnatanwfc@live.com>
2022-10-31 22:29:13 -0300

    makign circle

--- falling_over/main.py
+++ falling_over/main.py
@@ -7,19 +7,20 @@ WIN_WIDTH = 400
 WIN_HEIGHT = 800
 BG_COLOR = (0, 160, 80)
 
-CIRCLE_RADIOUS = 25
+CIRCLE_RADIUS = 25
 CIRCLE_CX = WIN_WIDTH / 2
 CIRCLE_CY = (WIN_HEIGHT / 2) + (WIN_HEIGHT / 4)
-CIRCLE_COLOR = (100, 10, 10)
+CIRCLE_COLOR = (200, 200, 200)
 
 def makeCircle(r, cx, cy):
-  circle = pg.Rect(cx - r, cy - r, r * 2, r * 2)
+  circle = pg.rect.Rect(0, 0, r * 2, r * 2)
+  circle.center = cx, cy 
   return circle 
 
 
 
 def main():
-  circle = makeCircle(CIRCLE_RADIOUS, CIRCLE_CX, CIRCLE_CY)
+  circle = makeCircle(CIRCLE_RADIUS, CIRCLE_CX, CIRCLE_CY)
   pg.display.init()
   screen = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
   while True:
@@ -32,6 +33,7 @@ def main():
         if event.key == pg.K_ESCAPE:
           pg.quit()
           sys.exit()
+    pg.draw.circle(screen, CIRCLE_COLOR, circle.center, CIRCLE_RADIUS)
     pg.display.flip()
     
   
