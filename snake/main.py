48815e541d8d8216375b4f84df1de20846e6c76b
ruby <jhonnatanwfc@live.com>
2022-11-02 02:15:28 -0300

    food

--- snake/main.py
+++ snake/main.py
@@ -20,13 +20,19 @@ class Snake:
     self.size = size 
     self.win = win
     self.vel = speed
-    self.body = []
     self.x_max = win.w // size 
     self.y_max = win.h // size 
+    self.dir = 'left'
+    self._makeBody()
+    self.last = self.body[-1]
+  
+  def _makeBody(self, length=4):
+    self.body = []
     headx, heady = self._getXY()
     self.body.append([headx, heady])
-    self.dir = random.choice(['left', 'right', 'up', 'down'])
-  
+    for i in range(1, length):
+      self.body.append([headx + i, heady])
+     
   def draw(self, surf):
     for x, y in self.body:
       left, top = self._getTopLeft(x, y)
@@ -51,23 +57,58 @@ class Snake:
       x = self.x_max
     elif x > self.x_max:
       x = 0
-    self.body[0] = [x, y]
+    self.body.insert(0, [x, y])
+    self.last = self.body[-1]
+    del self.body[-1]
+  
+  def _getHeadRect(self):
+    left, top = self.body[0]
+    return pygame.Rect(left, top, self.size, self.size)
+    
+  def gotFood(self, food):
+    #head = self._getHeadRect()
+    #if head.colliderect(foodRect):
+    x, y = self.body[0]
+    if food.x == x and food.y == y:
+      self.body.append(self.last)
+      return True 
+    return False          
     
   def _getXY(self):
     x = random.randint(0, self.x_max)
     y = random.randint(0, self.y_max)
-    return x, y 
+    return [x, y] 
   
   def _getTopLeft(self, x, y):
     left = x * self.size 
     top = y * self.size 
     return left, top 
+
+class Food:
+  def __init__(self, size, xmax, ymax):
+    self.s = size 
+    self.xmax = xmax 
+    self.ymax = ymax 
+    self.newPos()
   
+  def newPos(self):
+    self.x = random.randint(0, self.xmax)
+    self.y = random.randint(0, self.ymax)
+  
+  def getRect(self):
+    return pygame.Rect(self.s * self.x, self.s * self.y, self.s, self.s)
+  
+  def draw(self, surf):
+    rect = self.getRect()
+    pygame.draw.rect(surf, 'white', rect)
+    
+
 def main():
   FPS = 5
   clock = pygame.time.Clock()
   screen = Screen(800, 800, 'grey')
   snake = Snake(40, screen)
+  food = Food(snake.size, snake.x_max, snake.y_max)
   flag = True  
   while flag:
     clock.tick(FPS)
@@ -87,6 +128,9 @@ def main():
         elif event.key in (pygame.K_s, pygame.K_DOWN) and not snake.dir == 'up':
           snake.dir = 'down'
     snake.update()
+    if snake.gotFood(food):
+      food.newPos()
+    food.draw(screen.surf)
     snake.draw(screen.surf)
     pygame.display.update()
   pygame.quit()
