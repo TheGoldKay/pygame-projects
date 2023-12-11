f6983b69d36151f7cb38025a3fc483430a906660
TheGoldKay <jhonnatanwfc@live.com>
2023-12-11 10:53:45 -0300

    forced resize

--- enclosing/main.py
+++ enclosing/main.py
@@ -1,27 +1,31 @@
-import pygame
+import pygame, sys, time
 
 pygame.init()
 
 WIN_WIDTH = 800
 WIN_HEIGHT = 600
+FLAGS = pygame.RESIZABLE
 
-screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption("Enclosing")
-
-def main():
+def main(screen_width=WIN_WIDTH, screen_height=WIN_HEIGHT, flags=FLAGS, timer=time.time(), wait=3):
+    screen = pygame.display.set_mode((screen_width, screen_height), flags)
+    pygame.display.set_caption("Enclosing")
     done = False
     clock = pygame.time.Clock()
 
-    while not done:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     done = True
-
+        #if time.time() - timer > wait:
+        #    pygame.quit()
+        #    main(screen_width - 1, screen_height - 1)
+        if done:
+            pygame.quit()
+            sys.exit()
         screen.fill((18, 53, 36))
-
         pygame.display.flip()
         clock.tick(60)
 
