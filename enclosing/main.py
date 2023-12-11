c2035640c7898f5b706454bd8c0056f13443e8ea
TheGoldKay <jhonnatanwfc@live.com>
2023-12-11 17:22:04 -0300

    windows DLL/ctypes manipulation

--- enclosing/main.py
+++ enclosing/main.py
@@ -1,6 +1,8 @@
 from screeninfo import get_monitors
 import ctypes
 import pygame, sys, time, os, math
+from ctypes import POINTER, WINFUNCTYPE, windll
+from ctypes.wintypes import BOOL, HWND, RECT
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 500
@@ -18,7 +20,23 @@ def changeWinSize(x, y, new_width, new_height, hwnd):
         0  # Flags
     )
 
-def main(screen_width=WIN_WIDTH, screen_height=WIN_HEIGHT, flags=FLAGS, timer=time.time(), wait=0.07):
+def protoType():
+    # get our window ID:
+    hwnd = pygame.display.get_wm_info()["window"]
+
+    # Jump through all the ctypes hoops:
+    prototype = WINFUNCTYPE(BOOL, HWND, POINTER(RECT))
+    paramflags = (1, "hwnd"), (2, "lprect")
+
+    GetWindowRect = prototype(("GetWindowRect", windll.user32), paramflags)
+
+    # finally get our data!
+    rect = GetWindowRect(hwnd)
+    print ("top (%d), left (%d), bottom (%d), right (%d)" % (rect.top, rect.left, rect.bottom, rect.right))
+
+    # bottom, top, left, right:  644 98 124 644
+
+def main(screen_width=WIN_WIDTH, screen_height=WIN_HEIGHT, flags=FLAGS, timer=time.time(), wait=0.005):
     monitors = get_monitors() # Get the resolution of all of the users monitors
     for monitor in monitors:
         if monitor.is_primary: # main monitor
@@ -45,14 +63,15 @@ def main(screen_width=WIN_WIDTH, screen_height=WIN_HEIGHT, flags=FLAGS, timer=ti
                 if event.key == pygame.K_ESCAPE:
                     done = True
         if time.time() - timer > wait:
-            new_width, new_height = math.floor(screen_width * 0.999999), math.floor(screen_height * 0.999999)
+            new_width, new_height = math.floor(screen_width * 0.9999999), math.floor(screen_height * 0.9999999)
             new_pos_x = (pos_x + screen_width // 2) - new_width // 2
             new_pos_y = (pos_y + screen_height // 2) - new_height // 2
             changeWinSize(new_pos_x, new_pos_y, new_width, new_height, hwnd['window'])
             screen_width, screen_height = new_width, new_height
-            pox = new_pos_x
+            pos_x = new_pos_x
             pos_y = new_pos_y
             timer = time.time()
+            protoType()
         if done:
             pygame.quit()
             sys.exit()
